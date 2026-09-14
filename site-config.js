d7f3130708538b4afb3630bc0a8e3a48899ba022
Kaisheng Zhou <zhou2577@ahl02.agate.msi.umn.edu>
2026-09-14 09:08:57 -0500

    uplod new ndoes

--- site-config.js
+++ site-config.js
@@ -1,1 +1,1 @@
-window.HACO_STATIC = {"run_tags":["SPIRAL","SPIRAL2"],"default_run":"SPIRAL2","data_root":"data","inline":false,"generated_at":"2026-09-13T16:34:12.996466+00:00","title":"SPIRAL campaign"};
+window.HACO_STATIC = {"run_tags":["SPIRAL","SPIRAL2"],"default_run":"SPIRAL2","data_root":"data","inline":false,"generated_at":"2026-09-14T14:08:22.243930+00:00","title":"How SPIRAL searched for molecules"};
